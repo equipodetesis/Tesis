e209292c815787aabd08f487cd4fecf17f524387
Mario Eliezer Cisneros <marioeliezercb@outlook.es>
2019-10-27 16:46:44 -0600

    frestore cosas

--- lib/ListController.dart
+++ lib/ListController.dart
@@ -1,68 +1,120 @@
 import 'package:flutter/material.dart';
 import 'ListItem.dart';
 import 'BaseFormularios.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+// ignore: must_be_immutable
 class ListController extends StatelessWidget{
   List<ListItem> preitems=new List<ListItem>();
   bool search;
   String searchtext;
-  ListController( List<ListItem> items, bool search,String searchtext){
+  Firestore database;
+  ListController( List<ListItem> items, bool search,String searchtext,Firestore database){
     this.preitems=items;
     this.search=search;
     this.searchtext=searchtext;
+    this.database=database;
   }
 
   @override
   Widget build(BuildContext context) {
-    List<ListItem> items= new List<ListItem>();
+    Stream<QuerySnapshot> stream;
+    stream=database.collection('Clientes').snapshots();
 
-    if(search){
-      if(!searchtext.isEmpty)
-      for(int i=0;i<preitems.length;i++){
-        if(preitems[i].Nombre.toLowerCase().contains(searchtext.toLowerCase())){
-          items.add(preitems[i]);
+    return  StreamBuilder<QuerySnapshot>(
+      stream: stream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
+        if (snapshot.hasError)
+          return new Text('Error: ${snapshot.error}');
+        switch (snapshot.connectionState){
+          case ConnectionState.waiting: return Center(child:CircularProgressIndicator(
+            backgroundColor: Colors.deepOrange,
+          ));
+          break;
+          default:
+            return new ListView(
+                children: snapshot.data.documents.map((DocumentSnapshot document){
+                  ListItem item= ListItem("https://firebasestorage.googleapis.com/v0/b/expedientes-odontologicos.appspot.com/o/54462699_10214142660262421_7801861136030105600_n.jpg?alt=media&token=5060a01b-917e-42e5-ac5c-6cd8bff61f3b",
+                      document["Nombre"],document["edad"]);
+                  return
+                    searchtext==null||searchtext==""? Column(
+                      children: <Widget>[
+                        Divider(color: Colors.black,
+                          thickness: 0.12,
+                        ),
+                        ListTile(
+                            dense: false,
+
+                            leading: CircleAvatar(
+                              foregroundColor: Theme.of(context).primaryColor,
+                              backgroundColor: Colors.lightBlueAccent,
+                              backgroundImage: NetworkImage(item.imagen),
+                            ),
+                            title: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(
+                                  item.Nombre,
+                                  style:  TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                            onTap: (){
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => Baseformularios(item)),
+                              );},
+                            subtitle: Container(
+                              padding: const EdgeInsets.only(top: 5.0),
+                              child:  Text(
+                                item.fechacita,
+                                style:  TextStyle(color: Colors.grey, fontSize: 15.0),
+                              ),
+                            )
+                        )
+                      ],
+                    ):item.Nombre.toLowerCase().contains(searchtext.toLowerCase())?Column(
+                      children: <Widget>[
+                        Divider(color: Colors.black,
+                          thickness: 0.12,
+                        ),
+                        ListTile(
+                            dense: false,
+
+                            leading: CircleAvatar(
+                              foregroundColor: Theme.of(context).primaryColor,
+                              backgroundColor: Colors.lightBlueAccent,
+                              backgroundImage: NetworkImage(item.imagen),
+                            ),
+                            title: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(
+                                  item.Nombre,
+                                  style:  TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                            onTap: (){
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => Baseformularios(item)),
+                              );},
+                            subtitle: Container(
+                              padding: const EdgeInsets.only(top: 5.0),
+                              child:  Text(
+                                item.fechacita,
+                                style:  TextStyle(color: Colors.grey, fontSize: 15.0),
+                              ),
+                            )
+                        )
+                      ],
+                    )
+                        :Container();
+                }).toList()
+            );
         }
-      }
-      else
-        items=preitems;
-    }
-    else
-      items=preitems;
-
-    return ListView.builder(
-        itemCount: items.length,
-        itemBuilder:(context,index)=> Column(
-          children: <Widget>[
-            Divider(height: 10.0),
-            ListTile(
-                leading: CircleAvatar(
-                  foregroundColor: Theme.of(context).primaryColor,
-                  backgroundColor: Colors.lightBlueAccent,
-                  backgroundImage: AssetImage('Imagenes/incognito.png'),
-                ),
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      items[index].Nombre,
-                      style:  TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-                onTap: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Baseformularios(items[index])),
-                  );},
-                subtitle: Container(
-                  padding: const EdgeInsets.only(top: 5.0),
-                  child:  Text(
-                    items[index].fechacita,
-                    style:  TextStyle(color: Colors.grey, fontSize: 15.0),
-                  ),
-                )
-            ) ],
-
-        )
+      },
     );
+
   }
 }
--- lib/principal.dart
+++ lib/principal.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'ListItem.dart' as lista;
 import 'ListController.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class principal extends StatefulWidget{
   final String title;
   final List<lista.ListItem> items;
@@ -14,6 +16,8 @@ class principal extends StatefulWidget{
 class PrincipalState extends State<principal> {
   final String title;
   final List<lista.ListItem> items;
+  Firestore database=Firestore.instance;
+
   Icon actionIcon= Icon(Icons.search,color: Colors.white);
   bool _IsSearching;
   String _searchText = "";
@@ -43,44 +47,52 @@ class PrincipalState extends State<principal> {
   void initState() {
     super.initState();
     _IsSearching = false;
+    database.settings(persistenceEnabled: true);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){},
+        hoverColor: Colors.black,
+        isExtended: true,
+        tooltip: "Agregar nuevo paciente",
+      ),
       appBar: AppBar(
         title: appBarTitle,
         actions: <Widget>[
-            IconButton(
-              icon: actionIcon,
-              onPressed: (){
-                setState(() {
-                  if (this.actionIcon.icon == Icons.search) {
-                    this.actionIcon =  Icon(Icons.close, color: Colors.white,);
-                    this.appBarTitle =  TextField(
-                      controller: _searchQuery,
-                      style: new TextStyle(
-                        color: Colors.white,
+          IconButton(
+            icon: actionIcon,
+            onPressed: (){
+              setState(() {
+                if (this.actionIcon.icon == Icons.search) {
+                  this.actionIcon =  Icon(Icons.close, color: Colors.white,);
+                  this.appBarTitle =  TextField(
+                    controller: _searchQuery,
+                    style: new TextStyle(
+                      color: Colors.white,
 
-                      ),
-                      decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.search, color: Colors.white),
-                          hintText: "Buscar...",
-                          hintStyle:  TextStyle(color: Colors.white)
-                      ),
-                    );
-                    _handleSearchStart();
+                    ),
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.search, color: Colors.white),
+                        hintText: "Buscar...",
+                        hintStyle:  TextStyle(color: Colors.white)
+                    ),
+                  );
+                  _handleSearchStart();
                 }
-                  else{
-                    _handleSearchEnd();
-                  }
+                else{
+                  _handleSearchEnd();
                 }
-                );
-              },
-            )
+              }
+              );
+            },
+          )
         ],
       ),
-      body: ListController(items, _IsSearching,_searchText),
+      body: ListController(items, _IsSearching,_searchText,database),
     );
   }
   void _handleSearchStart() {
@@ -93,9 +105,10 @@ class PrincipalState extends State<principal> {
     setState(() {
       this.actionIcon =Icon(Icons.search, color: Colors.white,);
       this.appBarTitle =
-      Text("Historias clinicas", style:  TextStyle(color: Colors.white),);
+          Text("Historias clinicas", style:  TextStyle(color: Colors.white),);
       _IsSearching = false;
       _searchQuery.clear();
     });
   }
+
 }
